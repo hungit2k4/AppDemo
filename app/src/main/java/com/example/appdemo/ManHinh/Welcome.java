04294f26c5ee86593bade5d8ae07b96a6e024863
vunguyen <VuNguyenPoly@>
2023-07-30 13:36:05 +0700

    update code

--- app/src/main/java/com/example/appdemo/ManHinh/Welcome.java
+++ app/src/main/java/com/example/appdemo/ManHinh/Welcome.java
@@ -2,7 +2,9 @@ package com.example.appdemo.ManHinh;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.example.appdemo.R;
 
@@ -12,5 +14,13 @@ public class Welcome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Welcome.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        }, 3000);
     }
 }
