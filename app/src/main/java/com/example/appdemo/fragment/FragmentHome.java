4765e40fc5d99061f137efca71b933f5d03b5e34
hungit2k4 <vg.thanhson@gmail.com>
2023-08-15 12:55:41 +0700

    sua cn

--- app/src/main/java/com/example/appdemo/fragment/FragmentHome.java
+++ app/src/main/java/com/example/appdemo/fragment/FragmentHome.java
@@ -17,14 +17,13 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 import com.example.appdemo.Apdapter.QuanLyNhanVienAdapter;
-import com.example.appdemo.ManHinh.DangKyNghi;
-import com.example.appdemo.ManHinh.Fragment_ChamCong;
-import com.example.appdemo.ManHinh.Infor_account;
+import com.example.appdemo.ManHinh.InputCodeActivity;
 import com.example.appdemo.ManHinh.LoginActivity;
 import com.example.appdemo.ManHinh.QuanLyNhanVien;
 import com.example.appdemo.ManHinh.Trang_Chu;
 import com.example.appdemo.R;
 import com.example.appdemo.fragment.create_account.Fragment_Create_Account;
+import com.example.appdemo.models.Account;
 
 import java.util.ArrayList;
 
@@ -51,32 +50,31 @@ public class FragmentHome extends Fragment {
         btnCaNhan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), Infor_account.class);
-                startActivity(intent);
+
             }
         });
         btnChamCong.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Fragment fragment = new Fragment_ChamCong();
 
-                Trang_Chu.rltWelcome.setVisibility(View.GONE);
-
-                getActivity().getSupportFragmentManager().beginTransaction().
-                        replace(R.id.frLayout, fragment).addToBackStack(null).commit();
             }
         });
         btnDKNghi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-            Intent intent = new Intent(getContext(), DangKyNghi.class);
-            startActivity(intent);
+
             }
         });
         btnDoiMK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(getContext(), InputCodeActivity.class);
+                Account account= LoginActivity.account;
+                intent.putExtra("key", account.getUsername());
+                intent.putExtra("fullName", account.getFullName());
+                intent.putExtra("user", account.getUsername());
+                intent.putExtra("pass", account.getPassword());
+                startActivity(intent);
             }
         });
         btnCapTk.setOnClickListener(new View.OnClickListener() {
